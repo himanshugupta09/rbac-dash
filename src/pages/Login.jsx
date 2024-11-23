84436bdf807176a854703783b5c35c98cb11d69f
Himanshu Gupta <107400207+himanshugupta09@users.noreply.github.com>
2024-11-23 20:37:52 +0530

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useAuth } from "../auth/AuthProvider";
+
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+  const { login } = useAuth();
+
+  const handleChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(credentials);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
